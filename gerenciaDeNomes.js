001d57f5696d4e02e51f3d400e8bf3faa74f6a01
DeividiOliver <deividifirenze@gmail.com>
2022-11-03 17:05:32 -0300

    alteracoes

--- gerenciaDeNomes.js
+++ gerenciaDeNomes.js
@@ -1,9 +1,10 @@
 
-function juntarDoisNomes(primeiroNome, sobrenome) {
-    return `&{primeiroNome} OUTRA COISA &{sobrenome}`
+function juntarDoisNomes(primeiroNome, sobrenome) { 
+    return `${primeiroNome} OUTRA COISA ${sobrenome}`
 }
 function nomeEhValido(nome) {
     return nome != ""
 }
 
-
+module.exports.juntarDoisNomes = juntarDoisNomes
+module.exports.nomeEhValido = nomeEhValido
--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 import "./style.scss"
+const gerenciaDeNomes = require("./js/gerenciaDeNomes")
+
 console.log ("Olá mundo show")
+
+function executar_01() {
+    alert ("FUI DISPARADO")
+}
+
+const nome = document.querySelector("#nome").value
+const sobrenome = document.querySelector("sobrenome").value
+
+gerenciaDeNomes.juntarDoisNomes(nome, sobrenome)
+
+const botao_01 = document.querySelector("#botao_ex_01")
+botao_01.addEventListener("click", executar_01)
