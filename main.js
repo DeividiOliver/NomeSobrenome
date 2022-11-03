1fe1f42959188f7ec3ab26d00e3149ca1b8dd47a
DeividiOliver <deividifirenze@gmail.com>
2022-11-03 19:41:40 -0300

    alteracao feito para finalzar

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 import "./style.scss"
 const gerenciaDeNomes = require("./gerenciaDeNomes")
 
-console.log ("Olá mundo show")
-
-function executar_01() {
-
-const nome = document.querySelector("#nome").value
-const sobrenome = document.querySelector("#sobrenome").value
-
-const resultado = gerenciaDeNomes.juntarDoisNomes(nome, sobrenome)
-
-
-const divResultado = document.querySelector("#resultado_01")
-divResultado.innerHTML = resultado
-
+console.log("Olá mundo show")
+const botao_01 = document.querySelector("#botao_ex_01")
 botao_01.addEventListener("click", executar_01)
 
+function executar_01() {
+    alert('FUNCIONOU')
+    const nome = document.querySelector("#nome").value
+    const sobrenome = document.querySelector("#sobrenome").value
+
+    const resultado = gerenciaDeNomes.juntarDoisNomes(nome, sobrenome)
+
+
+    const divResultado = document.querySelector("#resultado_01")
+    divResultado.innerHTML = resultado
 }
